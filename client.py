b4c4ca7ec5cf35979d15f431db0783de1e040dad
Amit Shina <amitsh1808@gmail.com>
2025-09-23 18:24:10 +0300

    Split into multiple files

---
+++ client.py
@@ -0,0 +1,131 @@
+import socket
+import tkinter as tk
+from PIL import Image, ImageTk
+import io
+import struct
+import time
+
+
+class Client:
+    def __init__(self, port=5000, host=''): # Defualt port: 5000
+        if not host: host = input("Enter your IP Address: ")
+        
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
+        
+        # Recives and sets server screen resolution
+        data = self.sock.recv(8)
+        self.server_width = int.from_bytes(data[:4], 'big')
+        self.server_height = int.from_bytes(data[4:], 'big')
+        print(f"Server resolution: {self.server_width}x{self.server_height}")
+
+        self.root = tk.Tk() 
+        
+        self.root.geometry(f"{self.server_width}x{self.server_height}")
+        self.root.resizable(False, False)  # Disable resizing
+        
+        self.label = tk.Label(self.root) 
+        self.label.pack()
+
+        # Commend Actions
+        self.label.bind("<Motion>", self.mouse_move)
+        self.label.bind("<Button-1>", self.mouse_click)
+        self.label.bind("<Button-3>", self.mouse_click)
+        self.label.bind("<ButtonPress-1>", self.left_down)
+        self.label.bind("<ButtonRelease-1>", self.left_up)
+        self.label.bind("<KeyPress>", self.keyboard_down) 
+        self.label.bind("<KeyRelease>", self.keyboard_up)
+        
+        self.label.focus_set()
+        
+        self.last_sent = 0
+        
+        # Update Screen
+        self.update_frame()
+        self.root.mainloop()
+
+    def update_frame(self):
+        # Recives image size
+        size_data = self.sock.recv(8)
+        if not size_data:
+            self.root.destroy()
+            return
+        size = int.from_bytes(size_data, 'big')
+
+        # Receive image bytes
+        data = b''
+        while len(data) < size:
+            packet = self.sock.recv(size - len(data))
+            if not packet:
+                return
+            data += packet
+
+        img = Image.open(io.BytesIO(data))
+        img_tk = ImageTk.PhotoImage(img)
+
+        self.label.config(image=img_tk)
+        self.label.image = img_tk
+
+        self.root.after(30, self.update_frame)  # ~30 FPS
+
+    def mouse_move(self, event):
+        now = time.time()
+        if now - self.last_sent < 0.07:
+            return
+        self.last_sent = now
+        
+        try:
+            self.sock.sendall(b'MMouse' + struct.pack(">II", event.x, event.y).ljust(32, b'\x00'))
+        except:
+            pass
+    
+    # When clicking the mouse
+    def mouse_click(self, event):
+        try:
+            if event.num == 1:  # left click
+                self.sock.sendall(b'LMouse' + b'\x00'*32)
+            elif event.num == 3:  # right click
+                self.sock.sendall(b'RMouse' + b'\x00'*32)
+        except:
+            pass
+    
+    # When selectiong stuff with the mouse:
+    def left_down(self, event):
+        try:
+            self.sock.sendall(b'DMouse' + b'\x00'*32)
+        except:
+            pass
+    def left_up(self, event):
+        try:
+            self.sock.sendall(b'UMouse' + b'\x00'*32)
+        except:
+            pass
+    
+    def get_key(self, event):
+        key = event.char
+        if not key or not key.isprintable():
+            key = event.keysym
+        # print(f"char: '{event.char}', and keysym: '{event.keysym}', chosen: '{key}'")
+        return key
+    
+    def keyboard_down(self,event):
+        key = self.get_key(event)
+        try:
+            key_bytes = key.encode('utf-8')[:32].ljust(32, b'\x00')
+            self.sock.sendall(b'DKeybr' + key_bytes)
+            print("[SEND DOWN]", key)
+        except:
+            pass
+    
+    def keyboard_up(self,event):
+        key = self.get_key(event)
+        try:
+            key_bytes = key.encode('utf-8')[:32].ljust(32, b'\x00') 
+            self.sock.sendall(b'UKeybr' + key_bytes)
+            print("[SEND UP]", key) 
+        except:
+            pass
+
+def start_client(): Client()
+
+if __name__=="__main__": Client()
